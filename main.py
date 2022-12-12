ee9d6b14f661beb3334ce4ce7a3d32b3489b8fe0
iYasha <33287747+iYasha@users.noreply.github.com>
2022-12-12 20:52:21 +0200

    Add main.py file

---
+++ main.py
@@ -0,0 +1,41 @@
+
+# a = 4
+# b = 10
+#
+# if 5 < a > 20:  # a > 5 and a < 20:
+#     print('Yes')
+# elif 10 <= b <= 20:
+#     print('B')
+# else:
+#     print('No')
+
+# print('Hello')
+
+
+"""
+cd - change directory
+cd folder_name - move to folder_name directory
+cd .. - move to parent directory
+cd . - move to current directory
+cd ../new_folder - move to new_folder in parent directory
+cd C:/System/Users/Petya/Desktop/MyProject1 - absolute path
+"""
+
+"""
+Статусы файлов:
+1) Untracked
+2) Staged (git add file_name) or (git add *) | Remove file from staged: git rm --cached file_name
+3) Unmodified (git commit -m "commit message")
+4) Modified
+"""
+
+"""
+1) git status - Check repo status
+2) git add - Add file to commit
+3) git log - Commit history
+4) git commit -m "" - Create new commit -m (message)
+5) git restore file_name - Restore file to the last commit
+6) git push origin branch_name - Upload commit to remote repository
+7) git pull - retrieve commits from remote repository
+"""
+
